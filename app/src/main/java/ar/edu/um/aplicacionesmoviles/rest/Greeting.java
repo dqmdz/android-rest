df847a8e76a7e6d64d2e2ee13bb493f5b8341dd3
dqmdz <daniel.quinterospinto@gmail.com>
2016-09-30 08:50:38 -0300

    first commit

---
+++ app/src/main/java/ar/edu/um/aplicacionesmoviles/rest/Greeting.java
@@ -0,0 +1,17 @@
+package ar.edu.um.aplicacionesmoviles.rest;
+
+/**
+ * Created by daniel on 17/09/16.
+ */
+public class Greeting {
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
---
+++ app/src/main/java/ar/edu/um/aplicacionesmoviles/rest/MainActivity.java
@@ -0,0 +1,51 @@
+package ar.edu.um.aplicacionesmoviles.rest;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new HttpRequestTask().execute();
+    }
+
+    private class HttpRequestTask extends AsyncTask<Void, Void, Greeting> {
+        @Override
+        protected Greeting doInBackground(Void... params) {
+            try {
+                final String url = "http://10.100.33.21:8080/greeting?name=Amanda";
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Greeting greeting = restTemplate.getForObject(url, Greeting.class);
+                return greeting;
+            } catch (Exception e) {
+                Log.e("MainActivity", e.getMessage(), e);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Greeting greeting) {
+            TextView greetingIdText = (TextView) findViewById(R.id.tv2);
+            TextView greetingContentText = (TextView) findViewById(R.id.tv4);
+            greetingIdText.setText(greeting.getId());
+            greetingContentText.setText(greeting.getContent());
+        }
+
+    }
+}
